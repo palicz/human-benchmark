ac23357790aaef25ee7868ee783bd5c706fa2295
zenzty <hdtoki@gmail.com>
2024-11-12 22:08:52 +0100

    feat: Regisztrációs folyamat implementálása

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { db } from "@/lib/db";
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -11,6 +14,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: "Érvénytelen regisztrációs adatok!"};
     }
+
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return { error: "Ez az email cím már használatban van!"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    // TODO: Megerősítő email küldése
 
     return { success: "Sikeres regisztráció!"};
 };
