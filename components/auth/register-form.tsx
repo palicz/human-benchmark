d4be11fe09301c04752de6512766300363c90bcf
zenzty <hdtoki@gmail.com>
2024-11-12 21:07:02 +0100

    feat: Bejelentkezési komponensek bővítése, regisztrációs oldal komponens hozzáadása

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -6,7 +6,7 @@ import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import { Input } from "@/components/ui/input"
 import {
     Form,
@@ -21,27 +21,28 @@ import { CardWrapper } from "@/components/auth/card-wrapper"
 import { Button } from "@/components/ui/button";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
-import { login } from "@/actions/login";
+import { register } from "@/actions/register";
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
     const [isPending, startTransition] = useTransition();
 
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
         defaultValues: {
             email: "",
             password: "",
+            name: "",
         }
     });
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
         setError("")
         setSuccess("")
 
         startTransition(() => {
-            login(values)
+            register(values)
             .then((data) => {
                 setError(data.error)
                 setSuccess(data.success)
@@ -51,9 +52,9 @@ export const LoginForm = () => {
 
     return (
         <CardWrapper
-        headerLabel="Bejelentkezés"
-        backButtonLabel="Még nincs fiókod?"
-        backButtonHref="/auth/register"
+        headerLabel="Regisztráció"
+        backButtonLabel="Már van fiókod?"
+        backButtonHref="/auth/login"
         showSocial>
             <Form {...form}>
                 <form 
@@ -61,6 +62,17 @@ export const LoginForm = () => {
                 className="space-y-6"
                 >
                     <div className="space-y-4">
+                    <FormField control={form.control} name="name" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Felhasználónév
+                                </FormLabel>
+                                <FormControl>
+                                    <Input {...field} disabled={isPending} placeholder="John Doe" type="text"/>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}/>
                         <FormField control={form.control} name="email" render={({ field }) => (
                             <FormItem>
                                 <FormLabel>
@@ -87,7 +99,7 @@ export const LoginForm = () => {
                     <FormError message={error}/>
                     <FormSuccess message={success}/>
                     <Button disabled={isPending} typeof="submit" className="w-full">
-                        Bejelentkezés
+                        Fiók létrehozása
                     </Button>
                 </form>
             </Form>
--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
         message: "Hibás jelszó!"
     })
 });
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Hibás email cím!"
+    }),
+    password: z.string().min(6, {
+        message: "A jelszónak legalább 6 karakterből kell állnia!"
+    }),
+    name: z.string().min(1, {
+        message: "Hibás felhasználónév!"
+    })
+});
